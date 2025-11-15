322b1d03e2a130fba12598e000207930a602c73d
jihanurrahman33 <jihanurrahman33@gmail.com>
2025-11-15 13:50:38 +0600

    firebase config and context provider added

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import app from "../firebase/firebase.config";
+
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth(app);
+
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+  const provider = new GoogleAuthProvider();
+  const googleSignin = () => {
+    return signInWithPopup(auth, provider);
+  };
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [auth]);
+
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    registerUser,
+    loginUser,
+    googleSignin,
+    logOut,
+  };
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
